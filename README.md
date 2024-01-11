af4c3f898c1abe295e0d9409b23586aca0051115
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-01-11 18:45:06 +0330

    Upload Generate fake Profile Project On Github

---
+++ README.md
@@ -0,0 +1,20 @@
+This Program Generates Fake Profiles For You For Example Fake Email , Fake Name
+
+Description Of Files
+
+fake_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python fake_linux.py
+
+fake_windows.py ===> For Windows
+
+For Run ===> python fake_windows.py
+
+Required packages For Python
+
+faker ===> To install => pip install faker
+
+pyfiglet ===> To Install => pip install pyfiglet
+
+termcolor2 ===> To Install => pip install termcolor2
+
---
+++ fake_windows.py
@@ -0,0 +1,43 @@
+# Generate Fake Name And Email
+
+# Library
+from faker import Faker
+from os import system as sys
+from pyfiglet import figlet_format
+from termcolor2 import colored
+
+# Clear Terminal
+sys('cls')
+
+# Intro
+print('                                                  ')
+print(colored(figlet_format('Generates Fake Profile'), color='cyan'))
+print(colored('======================================================================', color='green'))
+print('                                                  ')
+
+# While True
+while True:
+
+    # Call Faker
+    fake = Faker()
+
+    # Production
+    print(f"Your Fake Email ===> {fake.email()}")
+    print(f"Your Fake Name ===> {fake.name()}")
+
+    # Try Again
+    print('                                               ')
+    con = input(colored('Do you want try again [y/n] ===> ', color='blue'))
+
+    # IF For Try Again
+    if con == 'y' or con == 'Y':
+        continue
+
+    elif con == 'n' or con == 'N':
+        print(colored(
+            '======================================================', color='blue'))
+        print(colored(figlet_format('Good Luck'), color='light_red'))
+        break
+
+# Finish
+# Create By Moein Heshmati
